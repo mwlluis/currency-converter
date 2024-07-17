f486c14adc942704aa42b400421ad41719dd0847
mwlluis <mwlluis@hotmail.com>
2024-07-17 16:35:02 -0400

    found a free api for currency, will use this going forward, can display currency conversion rates, next need to add text identifyer - dynamically

--- currency.js
+++ currency.js
@@ -1,29 +1,63 @@
-const clave = "cur_live_1FdRL5JM5ABwJGoc24XBAbFcCzePGCpLTR8maXNi";
-const url = `https://api.currencyapi.com/v3/latest?apikey=${clave}`;
+//fetch from this url - no key required
+const url = `https://open.er-api.com/v6/latest/USD`;
+//create an object that you can add exchange rates to, once the data is fetched
+const Currencies = {};
+let countries = ["AUD", "CAD", "EUR", "GBP"];
+let exchangeBoard = document.getElementById("exchangeBoard");
 
+//fetch json data
 async function getData() {
   try {
     const response = await fetch(url);
+
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
     const currencies = await response.json();
-    let exchanges = currencies.data;
+    console.log(currencies);
 
-    // console.log(Object.keys(exchanges));
+    Currencies.AUD = currencies.rates.AUD;
+    Currencies.CAD = currencies.rates.CAD;
+    Currencies.EUR = currencies.rates.EUR;
+    Currencies.GBP = currencies.rates.GBP;
+    // Currencies.lastUpdate = currencies.time_last_update_unix;
+    // Currencies.nextUpdate = currencies.time_next_update_unix;
 
-    function markets(set) {
-      if ("AUD" in set) {
-        console.log(set.AUD);
-      } else if ("CAN" in set) {
-        console.log(set.CAN);
-      }
-    }
-    markets(exchanges);
+    setEntry(countries);
   } catch (error) {
     console.error(error.message);
   }
 }
 
+console.log(Currencies);
+
 getData();
+
+//constructing the currency board
+
+function setEntry(array) {
+  for (i = 0; i < array.length; i++) {
+    if (array[i] === "AUD") {
+      const newLi = document.createElement("li");
+      newLi.setAttribute("id", array[i]);
+      newLi.innerHTML = `<span>${Currencies.AUD}</span>`;
+      exchangeBoard.appendChild(newLi);
+    } else if (array[i] === "CAD") {
+      const newLi = document.createElement("li");
+      newLi.setAttribute("id", array[i]);
+      newLi.innerHTML = `<span>${Currencies.CAD}</span>`;
+      exchangeBoard.appendChild(newLi);
+    } else if (array[i] === "EUR") {
+      const newLi = document.createElement("li");
+      newLi.setAttribute("id", array[i]);
+      newLi.innerHTML = `<span>${Currencies.EUR}</span>`;
+      exchangeBoard.appendChild(newLi);
+    } else if (array[i] === "GBP") {
+      const newLi = document.createElement("li");
+      newLi.setAttribute("id", array[i]);
+      newLi.innerHTML = `<span>${Currencies.GBP}</span>`;
+      exchangeBoard.appendChild(newLi);
+    }
+  }
+}
