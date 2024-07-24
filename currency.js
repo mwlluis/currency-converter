034376ec337733dfa8917c8665d5cf5de11cb117
mwlluis <mwlluis@hotmail.com>
2024-07-24 17:22:43 -0400

    figured out the refactor error, now fixed and functional

--- currency.js
+++ currency.js
@@ -3,8 +3,8 @@ const url = `https://open.er-api.com/v6/latest/USD`;
 //create an object that you can add exchange rates to, once the data is fetched
 const Currencies = {};
 
-//so this is one difference
 let countries = [];
+let countryCurrencyValues = [];
 //this selects the element where the currencies will be displayed
 let exchangeBoard = document.getElementById("exchangeBoard");
 
@@ -21,93 +21,73 @@ async function getData() {
     const currencies = await response.json();
 
     // //selects out the exchange rates from the json object, so this is good currency key: exchange rate
-    let allCountriesObj = currencies.rates;
+    let Currencies = currencies.rates;
 
-    // //coverts the previous object to an array so I can select out the exchange rate by the country code
-    let allCountriesArr = Object.keys(allCountriesObj);
+    let allCountriesArrKeys = Object.keys(Currencies);
+    let allCountriesArrValues = Object.values(Currencies);
 
-    // //this makes the allCountriesArr array the countries array - this is not necessary
-    allCountriesArr.forEach((element) => countries.push(element));
+    allCountriesArrKeys.forEach((element) => countries.push(element));
+    allCountriesArrValues.forEach((element) =>
+      countryCurrencyValues.push(element)
+    );
 
-    Currencies.ARS = currencies.rates.ARS;
-    Currencies.AUD = currencies.rates.AUD;
-    Currencies.BRL = currencies.rates.BRL;
-    Currencies.CAD = currencies.rates.CAD;
-    Currencies.EUR = currencies.rates.EUR;
-    Currencies.GBP = currencies.rates.GBP;
-    Currencies.ZAR = currencies.rates.ZAR;
-    // Currencies.lastUpdate = currencies.time_last_update_unix;
-    // Currencies.nextUpdate = currencies.time_next_update_unix;
+    Currencies.lastUpdate = currencies.time_last_update_unix;
+    Currencies.nextUpdate = currencies.time_next_update_unix;
 
-    setEntry(countries);
+    Currencies.lastUpdateUtc = currencies.time_last_update_utc;
+    Currencies.nextUpdateUtc = currencies.time_next_update_utc;
+
+    setEntry(countries, allCountriesArrValues);
   } catch (error) {
     console.error(error.message);
   }
 }
 
-// console.log(Currencies);
-
 getData();
 
 //constructing the currency board, this doesn't run duplicates because it's not an event-driven action
 
-function setEntry(array) {
+function setEntry(array, array2) {
   for (i = 0; i < array.length; i++) {
     if (array[i] === "ARS") {
       const newLi = document.createElement("li");
       newLi.setAttribute("id", array[i]);
-      newLi.innerHTML = `<span>${array[i]}: ${Currencies.ARS.toFixed(
-        2
-      )}</span>`;
+      newLi.innerHTML = `<span>${array[i]}: ${array2[i].toFixed(2)}</span>`;
       exchangeBoard.appendChild(newLi);
     } else if (array[i] === "AUD") {
       const newLi = document.createElement("li");
       newLi.setAttribute("id", array[i]);
-      newLi.innerHTML = `<span>${array[i]}: ${Currencies.AUD.toFixed(
-        2
-      )}</span>`;
+      newLi.innerHTML = `<span>${array[i]}: ${array2[i].toFixed(2)}</span>`;
       exchangeBoard.appendChild(newLi);
     } else if (array[i] === "BRL") {
       const newLi = document.createElement("li");
       newLi.setAttribute("id", array[i]);
-      newLi.innerHTML = `<span>${array[i]}: ${Currencies.BRL.toFixed(
-        2
-      )}</span>`;
+      newLi.innerHTML = `<span>${array[i]}: ${array2[i].toFixed(2)}</span>`;
       exchangeBoard.appendChild(newLi);
     } else if (array[i] === "BZL") {
       const newLi = document.createElement("li");
       newLi.setAttribute("id", array[i]);
-      newLi.innerHTML = `<span>${array[i]}: ${Currencies.BZL.toFixed(
-        2
-      )}</span>`;
+      newLi.innerHTML = `<span>${array[i]}: ${array2[i].toFixed(2)}</span>`;
       exchangeBoard.appendChild(newLi);
     } else if (array[i] === "CAD") {
       const newLi = document.createElement("li");
       newLi.setAttribute("id", array[i]);
-      newLi.innerHTML = `<span>${array[i]}: ${Currencies.CAD.toFixed(
-        2
-      )}</span>`;
+      newLi.innerHTML = `<span>${array[i]}: ${array2[i].toFixed(2)}</span>`;
       exchangeBoard.appendChild(newLi);
     } else if (array[i] === "EUR") {
       const newLi = document.createElement("li");
       newLi.setAttribute("id", array[i]);
-      newLi.innerHTML = `<span>${array[i]}: ${Currencies.EUR.toFixed(
-        2
-      )}</span>`;
+      newLi.innerHTML = `<span>${array[i]}: ${array2[i].toFixed(2)}</span>`;
       exchangeBoard.appendChild(newLi);
     } else if (array[i] === "GBP") {
       const newLi = document.createElement("li");
       newLi.setAttribute("id", array[i]);
-      newLi.innerHTML = `<span>${array[i]}: ${Currencies.GBP.toFixed(
-        2
-      )}</span>`;
+      newLi.innerHTML = `<span>${array[i]}: ${array2[i].toFixed(2)}</span>`;
       exchangeBoard.appendChild(newLi);
     } else if (array[i] === "ZAR") {
       const newLi = document.createElement("li");
       newLi.setAttribute("id", array[i]);
-      newLi.innerHTML = `<span>${array[i]}: ${Currencies.ZAR.toFixed(
-        2
-      )}</span>`;
+      newLi.innerHTML = `<span>${array[i]}: ${array2[i].toFixed(2)}</span>`;
       exchangeBoard.appendChild(newLi);
     }
   }
@@ -127,13 +107,13 @@ conversionBtn.addEventListener("click", function (event) {
   //this selects the ul that will display the converted currency amounts
   let resultsList = document.getElementById("resultsList");
   //need to clear out the previously posted conversions
-  let objCurrToArr = Object.values(Currencies);
-
-  //does a mapped array return? how to pass in an object into a function
-  let objCurrToArrMapped = objCurrToArr.map((x) =>
-    Number((x * dollarValue).toFixed(2))
-  );
-
+  function objArrConverter(array) {
+    //does a mapped array return? how to pass in an object into a function
+    let objCurrToArrMapped = array.map((x) =>
+      Number((x * dollarValue).toFixed(2))
+    );
+    return objCurrToArrMapped;
+  }
   //this clears the previous conversions so no duplication
   resultsList.innerHTML = "";
 
@@ -141,42 +121,49 @@ conversionBtn.addEventListener("click", function (event) {
   //ok so I need to fix objCurrToArrMapped
   convHeading.textContent = `$${dollarValue} Dollars in Foreign Currency`;
   //so pass in the objCurrToArrMapped to the function
-  function setResults(array1, array2) {
+  function setResults(array1, array2, objArrConverter) {
+    let array3 = objArrConverter(array2);
+
     for (i = 0; i < array1.length; i++) {
       if (array1[i] === "ARS") {
         const newConLi = document.createElement("li");
         // newConLi.setAttribute("id", array[i]);
-        newConLi.innerHTML = `<span>${array1[i]}: ${array2[i]} </span>`;
+        newConLi.innerHTML = `<span>${array1[i]}: ${array3[i]} </span>`;
         resultsList.appendChild(newConLi);
       } else if (array1[i] === "AUD") {
         const newConLi = document.createElement("li");
         // newConLi.setAttribute("id", array[i]);
-        newConLi.innerHTML = `<span>${array1[i]}: ${array2[i]}</span>`;
+        newConLi.innerHTML = `<span>${array1[i]}: ${array3[i]}</span>`;
         resultsList.appendChild(newConLi);
       } else if (array1[i] === "BRL") {
         const newConLi = document.createElement("li");
         // newConLi.setAttribute("id", array[i]);
-        newConLi.innerHTML = `<span>${array1[i]}: ${array2[i]}</span>`;
+        newConLi.innerHTML = `<span>${array1[i]}: ${array3[i]}</span>`;
         resultsList.appendChild(newConLi);
       } else if (array1[i] === "CAD") {
         const newConLi = document.createElement("li");
         // newConLi.setAttribute("id", array[i]);
-        newConLi.innerHTML = `<span>${array1[i]}: ${array2[i]}</span>`;
+        newConLi.innerHTML = `<span>${array1[i]}: ${array3[i]}</span>`;
         resultsList.appendChild(newConLi);
       } else if (array1[i] === "EUR") {
         const newConLi = document.createElement("li");
         // newConLi.setAttribute("id", array[i]);
-        newConLi.innerHTML = `<span>${array1[i]}: ${array2[i]}</span>`;
+        newConLi.innerHTML = `<span>${array1[i]}: ${array3[i]}</span>`;
         resultsList.appendChild(newConLi);
       } else if (array1[i] === "GBP") {
         const newConLi = document.createElement("li");
         // newConLi.setAttribute("id", array[i]);
-        newConLi.innerHTML = `<span>${array1[i]}: ${array2[i]}</span>`;
+        newConLi.innerHTML = `<span>${array1[i]}: ${array3[i]}</span>`;
+        resultsList.appendChild(newConLi);
+      } else if (array1[i] === "ZAR") {
+        const newConLi = document.createElement("li");
+        // newConLi.setAttribute("id", array[i]);
+        newConLi.innerHTML = `<span>${array1[i]}: ${array3[i]}</span>`;
         resultsList.appendChild(newConLi);
       }
     }
   }
 
-  setResults(countries, objCurrToArrMapped);
+  setResults(countries, countryCurrencyValues, objArrConverter);
   document.getElementById("dollarValue").value = "";
 });
